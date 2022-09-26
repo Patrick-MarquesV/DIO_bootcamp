206d5b740d6714efab0989275381c705fb617115
Patrick-MarequesV <patrick_1997marques@hotmail.com>
2022-09-26 11:53:37 -0300

    Exercicio de sobrecarga de metodos

---
+++ metodosJavaAula2_Sobrecarga/src/exercicioSobrecargaQuadrilateros/Main.java
@@ -0,0 +1,11 @@
+package exercicioSobrecargaQuadrilateros;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Cálculo área qualidrateros notáveis:");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d, 5d);
+        Quadrilatero.area(7, 8, 9);
+        Quadrilatero.area(5f, 5f);
+   }
+}
---
+++ metodosJavaAula2_Sobrecarga/src/exercicioSobrecargaQuadrilateros/Quadrilatero.java
@@ -0,0 +1,30 @@
+package exercicioSobrecargaQuadrilateros;
+
+public class Quadrilatero {
+    public static void area(double lado){
+
+        System.out.printf("Área do quadrado: %.1f\n", (lado*lado));
+
+    }
+
+    public static void area(double lado1, double lado2){
+        
+        System.out.printf("Área do retângulo: %.1f\n", (lado1*lado2));
+
+    }
+
+    public static void area(double baseMaior, double baseMenor, double altura){
+
+        System.out.printf("Área do trapézio: %.1f\n", ((baseMaior+baseMenor)*altura)/2);
+
+    }
+
+    public static void area(float diagonalMenor, float diaginalMaior){
+
+        System.out.printf("Área do losângo: %.1f\n", (diaginalMaior*diagonalMenor)/2);
+
+    }
+
+    
+
+}
