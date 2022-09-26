521a872b7615c492cd24335f0b7d67fdad04e76a
Patrick-MarequesV <patrick_1997marques@hotmail.com>
2022-09-26 12:07:46 -0300

    Exercicio Aula 3 Retornos

---
+++ metodosJavaAula3_retorno/src/aula3Retornos/Main.java
@@ -0,0 +1,11 @@
+package aula3Retornos;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Cálculo área qualidrateros notáveis:");
+        System.out.println("Área do quadrado: " + Quadrilatero.area(3));
+        System.out.println("Área do retângulo: " + Quadrilatero.area(5d, 5d));
+        System.out.println("Área do trapézio: "+ Quadrilatero.area(7, 8, 9));
+        System.out.println("Área do losângo: " + Quadrilatero.area(5f, 5f));
+   }
+}
---
+++ metodosJavaAula3_retorno/src/aula3Retornos/Quadrilatero.java
@@ -0,0 +1,27 @@
+package aula3Retornos;
+
+public class Quadrilatero {
+    public static double area(double lado){
+
+        return (lado*lado);
+
+    }
+
+    public static double area(double lado1, double lado2){
+        
+        return (lado1*lado2);
+
+    }
+
+    public static double area(double baseMaior, double baseMenor, double altura){
+
+        return ((baseMaior+baseMenor)*altura)/2;
+
+    }
+
+    public static double area(float diagonalMenor, float diaginalMaior){
+
+        return (diaginalMaior*diagonalMenor)/2;
+
+    }
+}
